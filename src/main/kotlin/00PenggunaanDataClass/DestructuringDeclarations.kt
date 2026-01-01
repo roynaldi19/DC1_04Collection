cac1edb56429228236b9511854bb9b3254b79c08
roynaldi19 <roynaldi19@gmail.com>
2026-01-01 19:56:53 +0700

    update jan 26

--- src/main/kotlin/00PenggunaanDataClass/DestructuringDeclarations.kt
+++
@@ -1,8 +0,0 @@
-package `00PenggunaanDataClass`
-
-fun main() {
-    val dataUser = DataUser("Roy", 29)
-    val (name, age) = dataUser //mengambil berdasarkan urutan data
-
-    println("My name is $name, I am $age")
-}
